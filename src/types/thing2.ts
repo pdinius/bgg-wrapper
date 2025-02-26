b13b8aa90420270c3151038e37cdc764dca9b634
dinx <phildinius@gmail.com>
2025-02-26 08:11:13 -0500

    updates

---
+++ src/types/thing2.ts
@@ -0,0 +1,10 @@
+export interface ThingOptions {
+    versions: boolean;
+    videos: boolean;
+    stats: boolean;
+    marketplace: boolean;
+    comments: boolean;
+    ratings: boolean;
+    page: number;
+}
+
